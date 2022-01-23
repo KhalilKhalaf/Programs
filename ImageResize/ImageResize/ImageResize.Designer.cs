b4edddedc46909fc34afa7dbefc4e2a64140114c
Khalil Khalaf <khalafkhalil@gmail.com>
2022-01-22 19:32:29 -0500

    Done - Needs to expose desired values to the user

--- ImageResize/ImageResize/ImageResize.Designer.cs
+++ ImageResize/ImageResize/ImageResize.Designer.cs
@@ -30,82 +30,153 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageResize));
-            this.CamImageBox = new Emgu.CV.UI.ImageBox();
-            this.btnStartLiveVideo = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox)).BeginInit();
+            this.NewImageBox = new Emgu.CV.UI.ImageBox();
+            this.BrowseInputBtn = new System.Windows.Forms.Button();
+            this.BrowseOutputBtn = new System.Windows.Forms.Button();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.OldImageBox = new Emgu.CV.UI.ImageBox();
+            this.SubmitBtn = new System.Windows.Forms.Button();
+            this.IgnoreBtn = new System.Windows.Forms.Button();
+            this.OldDataGridView = new System.Windows.Forms.DataGridView();
+            this.AutoModeCheckbox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.NewImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OldImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OldDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // CamImageBox
+            // NewImageBox
             // 
-            this.CamImageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.CamImageBox.Location = new System.Drawing.Point(14, 3);
-            this.CamImageBox.Name = "CamImageBox";
-            this.CamImageBox.Size = new System.Drawing.Size(608, 433);
-            this.CamImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.CamImageBox.TabIndex = 2;
-            this.CamImageBox.TabStop = false;
+            this.NewImageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NewImageBox.Location = new System.Drawing.Point(766, 3);
+            this.NewImageBox.Name = "NewImageBox";
+            this.NewImageBox.Size = new System.Drawing.Size(680, 403);
+            this.NewImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.NewImageBox.TabIndex = 2;
+            this.NewImageBox.TabStop = false;
             // 
-            // btnStartLiveVideo
+            // BrowseInputBtn
             // 
-            this.btnStartLiveVideo.Location = new System.Drawing.Point(14, 444);
-            this.btnStartLiveVideo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnStartLiveVideo.Name = "BrowseOutputBtn";
-            this.btnStartLiveVideo.Size = new System.Drawing.Size(150, 35);
-            this.btnStartLiveVideo.TabIndex = 6;
-            this.btnStartLiveVideo.Text = "Browse Output";
-            this.btnStartLiveVideo.UseVisualStyleBackColor = true;
-            this.btnStartLiveVideo.Click += new System.EventHandler(this.BrowseOutputBtn_Click);
+            this.BrowseInputBtn.Location = new System.Drawing.Point(3, 612);
+            this.BrowseInputBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BrowseInputBtn.Name = "BrowseInputBtn";
+            this.BrowseInputBtn.Size = new System.Drawing.Size(150, 42);
+            this.BrowseInputBtn.TabIndex = 6;
+            this.BrowseInputBtn.Text = "Browse Input";
+            this.BrowseInputBtn.UseVisualStyleBackColor = true;
+            this.BrowseInputBtn.Click += new System.EventHandler(this.BrowseInputBtn_Click);
             // 
-            // button1
+            // BrowseOutputBtn
             // 
-            this.button1.Location = new System.Drawing.Point(181, 444);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button1.Name = "BrowseInputBtn";
-            this.button1.Size = new System.Drawing.Size(150, 35);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Browse Input";
-            this.button1.UseVisualStyleBackColor = true;
-            this.btnStartLiveVideo.Click += new System.EventHandler(this.BrowseInputBtn_Click);
-
+            this.BrowseOutputBtn.Location = new System.Drawing.Point(161, 612);
+            this.BrowseOutputBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BrowseOutputBtn.Name = "BrowseOutputBtn";
+            this.BrowseOutputBtn.Size = new System.Drawing.Size(150, 42);
+            this.BrowseOutputBtn.TabIndex = 7;
+            this.BrowseOutputBtn.Text = "Browse Output";
+            this.BrowseOutputBtn.UseVisualStyleBackColor = true;
+            this.BrowseOutputBtn.Click += new System.EventHandler(this.BrowseOutputBtn_Click);
             // 
-            // button2
+            // StartBtn
             // 
-            this.button2.Location = new System.Drawing.Point(472, 444);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button2.Name = "Start";
-            this.button2.Size = new System.Drawing.Size(150, 35);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Start";
-            this.button2.UseVisualStyleBackColor = true;
-            this.btnStartLiveVideo.Click += new System.EventHandler(this.StartBtn_Click);
-
+            this.StartBtn.Location = new System.Drawing.Point(319, 612);
+            this.StartBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(150, 42);
+            this.StartBtn.TabIndex = 8;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // OldImageBox
+            // 
+            this.OldImageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OldImageBox.Location = new System.Drawing.Point(12, 3);
+            this.OldImageBox.Name = "OldImageBox";
+            this.OldImageBox.Size = new System.Drawing.Size(680, 403);
+            this.OldImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.OldImageBox.TabIndex = 9;
+            this.OldImageBox.TabStop = false;
+            // 
+            // SubmitBtn
+            // 
+            this.SubmitBtn.Location = new System.Drawing.Point(1295, 615);
+            this.SubmitBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.SubmitBtn.Name = "SubmitBtn";
+            this.SubmitBtn.Size = new System.Drawing.Size(150, 42);
+            this.SubmitBtn.TabIndex = 10;
+            this.SubmitBtn.Text = "Submit";
+            this.SubmitBtn.UseVisualStyleBackColor = true;
+            this.SubmitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
+            // 
+            // IgnoreBtn
+            // 
+            this.IgnoreBtn.Location = new System.Drawing.Point(1137, 615);
+            this.IgnoreBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.IgnoreBtn.Name = "IgnoreBtn";
+            this.IgnoreBtn.Size = new System.Drawing.Size(150, 42);
+            this.IgnoreBtn.TabIndex = 11;
+            this.IgnoreBtn.Text = "Ignore";
+            this.IgnoreBtn.UseVisualStyleBackColor = true;
+            this.IgnoreBtn.Click += new System.EventHandler(this.IgnoreBtn_Click);
+            // 
+            // OldDataGridView
+            // 
+            this.OldDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.OldDataGridView.Location = new System.Drawing.Point(12, 412);
+            this.OldDataGridView.Name = "OldDataGridView";
+            this.OldDataGridView.RowHeadersWidth = 62;
+            this.OldDataGridView.RowTemplate.Height = 28;
+            this.OldDataGridView.Size = new System.Drawing.Size(1434, 195);
+            this.OldDataGridView.TabIndex = 12;
+            // 
+            // AutoModeCheckbox
+            // 
+            this.AutoModeCheckbox.AutoSize = true;
+            this.AutoModeCheckbox.Location = new System.Drawing.Point(477, 615);
+            this.AutoModeCheckbox.Name = "AutoModeCheckbox";
+            this.AutoModeCheckbox.Size = new System.Drawing.Size(113, 24);
+            this.AutoModeCheckbox.TabIndex = 13;
+            this.AutoModeCheckbox.Text = "Auto Mode";
+            this.AutoModeCheckbox.UseVisualStyleBackColor = true;
+            this.AutoModeCheckbox.CheckedChanged += new System.EventHandler(this.AutoModeCheckbox_CheckedChanged);
             // 
             // ImageResize
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(638, 485);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.btnStartLiveVideo);
-            this.Controls.Add(this.CamImageBox);
+            this.ClientSize = new System.Drawing.Size(1458, 668);
+            this.Controls.Add(this.AutoModeCheckbox);
+            this.Controls.Add(this.OldDataGridView);
+            this.Controls.Add(this.IgnoreBtn);
+            this.Controls.Add(this.SubmitBtn);
+            this.Controls.Add(this.OldImageBox);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.BrowseOutputBtn);
+            this.Controls.Add(this.BrowseInputBtn);
+            this.Controls.Add(this.NewImageBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ImageResize";
             this.Text = "Camera Output";
             this.Load += new System.EventHandler(this.ImageResize_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NewImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OldImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OldDataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Emgu.CV.UI.ImageBox CamImageBox;
-        private System.Windows.Forms.Button btnStartLiveVideo;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private Emgu.CV.UI.ImageBox OldImageBox;
+        private Emgu.CV.UI.ImageBox NewImageBox;
+        private System.Windows.Forms.Button BrowseInputBtn;
+        private System.Windows.Forms.Button BrowseOutputBtn;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button SubmitBtn;
+        private System.Windows.Forms.Button IgnoreBtn;
+        private System.Windows.Forms.DataGridView OldDataGridView;
+        private System.Windows.Forms.CheckBox AutoModeCheckbox;
     }
 }
 
--- ImageResize/ImageResize/ImageResize.cs
+++ ImageResize/ImageResize/ImageResize.cs
@@ -1,12 +1,9 @@
 ﻿using Emgu.CV;
 using Emgu.CV.Structure;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
 
 
@@ -14,29 +11,254 @@ namespace ImageResize
 {
     public partial class ImageResize : Form
     {
+        string mInputFolder = null;
+        string mOutputFolder = null;
+        string[] mAllInputFiles;
+        Image<Bgr, Byte> mCurImage;
+        string mCurImagePath;
+        int mCurImageIndex = -1;
+        string mTempDirectoryPath = string.Empty;
+
+        const int cDesiredHeight = 1200;
+        const int cDesiredWidth = 1800;
+        const double cReductionScale = 0.9;
+
+        DataTable mCommunicationTable;
+        bool mAutoMode = false;
+
         public ImageResize()
         {
             InitializeComponent();
         }
 
-        private void ImageResize_Load(object sender, EventArgs e)
+        private void ImageResize_Load( object sender, EventArgs e )
         {
+            BrowseInputBtn.Enabled = true;
+            BrowseOutputBtn.Enabled = false;
+            StartBtn.Enabled = false;
+            IgnoreBtn.Enabled = false;
+            SubmitBtn.Enabled = false;
+
+            mCurImageIndex = 0;
+
+            mCommunicationTable = new DataTable();
+            mCommunicationTable.Columns.Add( "Desc", typeof( string ) );
+            mCommunicationTable.Columns.Add( "Size", typeof( int ) );
+            mCommunicationTable.Columns.Add( "Height", typeof( int ) );
+            mCommunicationTable.Columns.Add( "Width", typeof( int ) );
+            mCommunicationTable.Columns.Add( "Path", typeof( string ) );
+
+            OldDataGridView.DataSource = mCommunicationTable;
+            OldDataGridView.Columns[0].Width = 50;
+            OldDataGridView.Columns[1].Width = 100;
+            OldDataGridView.Columns[2].Width = 50;
+            OldDataGridView.Columns[3].Width = 50;
+            OldDataGridView.Columns[4].Width = 1000;
+        }
+
+        private void BrowseInputBtn_Click( object sender, EventArgs e )
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Desktop;
+
+            if ( folderBrowserDialog1.ShowDialog() == DialogResult.OK )
+            {
+                mInputFolder = folderBrowserDialog1.SelectedPath;
+                // Maybe check mInputFolder validity
+
+                BrowseOutputBtn.Enabled = true;
+                BrowseInputBtn.Enabled = false;
+            }
 
         }
 
-        private void BrowseInputBtn_Click(object sender, EventArgs e)
+        private void BrowseOutputBtn_Click( object sender, EventArgs e )
         {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Desktop;
+
+            if ( folderBrowserDialog1.ShowDialog() == DialogResult.OK )
+            {
+                mOutputFolder = folderBrowserDialog1.SelectedPath;
+
+                // maybe validate mOutputFolder here
+
+                if ( mInputFolder != null )
+                {
+                    mCurImageIndex = 0;
+                    StartBtn.Enabled = true;
+                    BrowseOutputBtn.Enabled = false;
+
+                }
+            }
+        }
+
+        private void StartBtn_Click( object sender, EventArgs e )
+        {
+            if( mCurImageIndex == -1 )
+            {
+                return;
+            }
+
+            mCurImageIndex = 0;
+            BrowseInputBtn.Enabled = false;
+            BrowseOutputBtn.Enabled = false;
+            SubmitBtn.Enabled = true;
+            IgnoreBtn.Enabled = true;
+            StartBtn.Enabled = false;
+
+            mAllInputFiles = Directory.GetFiles( mInputFolder );
+
+            if(mAllInputFiles.Length == 0)
+            {
+                MessageBox.Show("No Files Found.");
+                StartBtn.Enabled = true;
+                mCurImageIndex = 0;
+                return;
+            }
+
+            ProcessNextImage();
+        }
+        
+        private void SetTemporaryDirectory()
+        {
+            if(mTempDirectoryPath == string.Empty)
+            {
+                // https://stackoverflow.com/questions/278439/creating-a-temporary-directory-in-windows
+                var uniqueTempDir = Path.GetFullPath(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString()));
+                Directory.CreateDirectory(uniqueTempDir + "\\");
+                mTempDirectoryPath = uniqueTempDir;
+
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    Arguments = mTempDirectoryPath,
+                    FileName = "explorer.exe"
+                };
+
+                Process.Start(startInfo);
+            }
 
         }
 
-        private void BrowseOutputBtn_Click(object sender, EventArgs e)
+        private void ProcessNextImage()
         {
 
+            int lNewHeight, lNewWidth;
+
+            if ( mCurImageIndex == mAllInputFiles.Length )
+            {
+                BrowseInputBtn.Enabled = true;
+                BrowseOutputBtn.Enabled = false;
+                StartBtn.Enabled = false;
+                IgnoreBtn.Enabled = false;
+                SubmitBtn.Enabled = false;
+                return;
+            }
+
+            mCurImage = new Image<Bgr, byte>( mAllInputFiles[mCurImageIndex] );
+            mCurImagePath = mAllInputFiles[mCurImageIndex];
+
+            if ( mCurImage.Height > mCurImage.Width )
+            {
+                lNewHeight = ( int )( mCurImage.Height * cReductionScale );
+                lNewWidth = ( int )( mCurImage.Width * cReductionScale );
+
+                while ( lNewHeight > cDesiredHeight )
+                {
+                    lNewHeight = ( int )( lNewHeight * cReductionScale );
+                    lNewWidth = ( int )( lNewWidth * cReductionScale );
+                }
+            }
+            else
+            {
+                lNewHeight = ( int )( mCurImage.Height * cReductionScale );
+                lNewWidth = ( int )( mCurImage.Width * cReductionScale );
+
+                while ( lNewWidth > cDesiredWidth )
+                {
+                    lNewWidth = ( int )( lNewWidth * cReductionScale );
+                    lNewHeight = ( int )( lNewHeight * cReductionScale );
+                }
+            }
+
+            OldImageBox.Image = mCurImage;
+            mCommunicationTable.Clear();
+
+            FileInfo fi = new FileInfo( mCurImagePath );
+            mCommunicationTable.Rows.Add( "Old:", fi.Length / 1024, mCurImage.Height, mCurImage.Width, mCurImagePath );
+
+            mCurImage = mCurImage.Resize( lNewWidth, lNewHeight, Emgu.CV.CvEnum.Inter.Cubic );
+            NewImageBox.Image = mCurImage;
+            
+            SetTemporaryDirectory();
+            string lCurImageNewPath = mTempDirectoryPath + "\\" + Path.GetFileName( mCurImagePath );
+            mCurImage.Save( lCurImageNewPath );
+            
+            fi = new FileInfo( lCurImageNewPath );
+            mCommunicationTable.Rows.Add( "New:", fi.Length / 1024, mCurImage.Height, mCurImage.Width, lCurImageNewPath );
+            mCommunicationTable.Rows.Add( "Num: ", mCurImageIndex + 1 / mAllInputFiles.Length);
+            
+            OldDataGridView.DataSource = mCommunicationTable;
+
+            if(mAutoMode)
+            {
+                ProcessPhysicalFile();
+                mCurImageIndex++;
+                ProcessNextImage();
+            }
         }
 
-        private void StartBtn_Click(object sender, EventArgs e)
+        private void ProcessPhysicalFile()
         {
+            // move from temp directory to output
+            string lCurImageNewPath = mOutputFolder + "\\" + Path.GetFileName( mCurImagePath );
+            string mSourcePath = mTempDirectoryPath + "\\" + Path.GetFileName(mCurImagePath);
+            string mDestinationPath = lCurImageNewPath;
 
+            if ( File.Exists(mDestinationPath) )
+            {
+                File.Delete( mDestinationPath );
+            }
+            File.Move( mSourcePath, mDestinationPath );
+
+            // move from input directory to sufolder "done"
+            mSourcePath = mCurImagePath;
+            mDestinationPath = mInputFolder + "\\Done\\";
+
+            if ( Directory.Exists(mDestinationPath) )
+            {
+                if(File.Exists(mDestinationPath + "\\" + Path.GetFileName(mCurImagePath)))
+                {
+                    File.Delete(mDestinationPath + "\\" + Path.GetFileName(mCurImagePath));
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(mDestinationPath);
+            }
+            
+
+            File.Move(mSourcePath, mDestinationPath + "\\" + Path.GetFileName(mCurImagePath) );
+        }
+
+        private void SubmitBtn_Click( object sender, EventArgs e )
+        {
+            ProcessPhysicalFile();
+            mCurImageIndex++;
+            ProcessNextImage();
+        }
+        
+        private void IgnoreBtn_Click( object sender, EventArgs e )
+        {
+            mCurImageIndex++;
+            ProcessNextImage();
+        }
+
+        private void AutoModeCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            mAutoMode = !mAutoMode;
         }
     }
 }
